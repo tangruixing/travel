a7991d4598bfdaaf88e80c0668da56eb2b31f127
我下午睡了很久 <1061595132@qq.com>
2015-06-16 08:32:18 +0800

    Signed-off-by: 我下午睡了很久 <1061595132@qq.com>

---
+++ travel/src/cn/model/Constant.java
@@ -0,0 +1,32 @@
+package cn.model;
+
+public class Constant {
+
+	/**
+	 * 管理员
+	 */
+	public static int ROLE_ADMIN=1;
+	/**
+	 * 普通用户
+	 */
+	public static int ROLE_MEMBER=2;
+	
+	
+	/**
+	 * 线路：针对普通用户
+	 */
+	public static int ROUTER_PAY=1;
+	/**
+	 * 线路：针对自由行用户
+	 */
+	public static int ROUTER_NOPAY=2;
+	
+	/**
+	 * 景区
+	 */
+	public static int scenery_parent=1;
+	/**
+	 * 景点
+	 */
+	public static int scenery_child=2;
+}
