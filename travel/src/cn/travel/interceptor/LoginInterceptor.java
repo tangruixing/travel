176a725889bba3dbd8ef02ddc1992f38ba74f1ee
guanxino <571271494@qq.com>
2015-06-18 11:49:56 +0800

    Signed-off-by: guanxino <571271494@qq.com>

---
+++ travel/src/cn/travel/interceptor/LoginInterceptor.java
@@ -0,0 +1,63 @@
+package cn.travel.interceptor;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import cn.travel.action.BaseAction;
+import cn.travel.action.LoginAction;
+import cn.travel.action.UserAware;
+import cn.travel.model.User;
+import cn.util.ConfigUtil;
+
+import com.alibaba.fastjson.JSON;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.ActionProxy;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class LoginInterceptor implements Interceptor{
+
+	Logger log=Logger.getLogger(this.getClass());
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8105910702540607936L;
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@SuppressWarnings("rawtypes")
+	public String intercept(ActionInvocation invocation) throws Exception {
+		// TODbO Auto-generated method stub
+		BaseAction action = (BaseAction) invocation.getAction();
+		ActionProxy proxy = invocation.getProxy();
+		
+		log.info("拦截器拦截的ActionName:"+proxy.getActionName()+",Method:"+proxy.getMethod());
+		if(action instanceof LoginAction){
+			log.info("直接放行..");
+			return invocation.invoke();
+		}else{
+			HttpSession session = ServletActionContext.getRequest().getSession();
+			User user = (User) session.getAttribute(ConfigUtil.loginUserKey);
+			if(user!=null){
+				//处理 user　注入
+				if(action instanceof UserAware){
+					((UserAware)action).setUser(user);
+				}
+				log.info(JSON.toJSONStringWithDateFormat(user, "yy-MM-dd HH:mm:ss"));
+				return invocation.invoke();
+			}
+			log.info("没有登录,跳转到登录页面....");
+			return "toLogin";
+		}
+	}
+
+}
